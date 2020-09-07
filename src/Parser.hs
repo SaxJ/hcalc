72614c8bdf8fec9f2c9d85a9cd2f4316ed20b7ec
Saxon Jensen <saxon.jensen@healthengine.com.au>
2020-09-07 21:49:20 +0800

    The rest of the parser

--- src/Parser.hs
+++ src/Parser.hs
@@ -1,4 +1,4 @@
-module Parser () where
+module Parser (eval) where
 
 import Control.Applicative
 import Data.Char
@@ -41,7 +41,108 @@ instance Alternative Parser where
                                   [] -> parse q input
                                   result -> result)
 
-single :: (Char -> Bool) -> Parser Char
-single p = do
+cond :: (Char -> Bool) -> Parser Char
+cond p = do
     x <- charP
     if p x then return x else empty
+
+-- different parsers
+digit :: Parser Char
+digit = cond isDigit
+
+lower :: Parser Char
+lower = cond isLower
+
+upper :: Parser Char
+upper = cond isUpper
+
+letter :: Parser Char
+letter = cond isAlpha
+
+alphanum :: Parser Char
+alphanum = cond isAlphaNum
+
+char :: Char -> Parser Char
+char x = cond (== x)
+
+string :: String -> Parser String
+string [] = return []
+string (x:xs) = do
+    char x
+    string xs
+    return (x:xs)
+
+ident :: Parser String
+ident = do
+    x <- lower
+    xs <- many alphanum
+    return (x:xs)
+
+nat :: Parser Int
+nat = do
+    xs <- some digit
+    return (read xs)
+
+space :: Parser ()
+space = do
+    many (cond isSpace)
+    return ()
+
+int :: Parser Int
+int = do
+    char '-'
+    n <- nat
+    return (-n)
+    <|> nat
+
+token :: Parser a -> Parser a
+token p = do
+    space
+    v <- p
+    space
+    return v
+
+-- now parsers that ignore spacing
+identifier :: Parser String
+identifier = token ident
+
+natural :: Parser Int
+natural = token nat
+
+integer :: Parser Int
+integer = token int
+
+symbol :: String -> Parser String
+symbol s = token (string s)
+
+expr :: Parser Int
+expr = do
+    t <- term
+    do
+        symbol "+"
+        e <- expr
+        return (t + e)
+        <|> return t
+
+term :: Parser Int
+term = do
+    f <- factor
+    do 
+        symbol "*"
+        t <- term
+        return (f * t)
+        <|> return f
+
+factor :: Parser Int
+factor = do
+    symbol "("
+    e <- expr
+    symbol ")"
+    return e
+    <|> natural
+
+eval :: String -> Int
+eval xs = case (parse expr xs) of
+            [(n, [])] -> n
+            [(_, out)] -> error ("Invalid input: " ++ out)
+            [] -> error "Invalid input"
